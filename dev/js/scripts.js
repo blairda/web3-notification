fd677f39deaddddd83e0c9f247deb384a36b196c
blairda <bda34@drexel.edu>
2020-08-10 21:03:13 -0400

    Add modal box notification/map

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,14 +1,26 @@
 
-// @codekit-prepend "nav-scroll.js";
-// @codekit-prepend "main-nav-click.js";
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-function notification() {
-    var txt;
-    if (confirm("Allow access to your location:")) {
-      txt = "Location access granted";
-    } else {
-      txt = "Location access denied";
-    }
-    document.getElementById("response").innerHTML = txt;
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
+}
